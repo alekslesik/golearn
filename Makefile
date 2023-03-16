524e03437ef0f3e5289d5e46dd38247adbfe222b
alekslesik <alekslesik@gmail.com>
2023-03-16 21:14:52 +0300

    Using in a web application

---
+++ Makefile
@@ -0,0 +1,50 @@
+#=====================================#
+# HELPERS #
+#=====================================#
+
+## help: print this help message
+.PHONY: help
+help:
+	@echo 'Usage:'
+	@sed -n 's/^##//p' ${MAKEFILE_LIST} | column -t -s ':' |  sed -e 's/^/ /'
+
+#=====================================#
+# DEVELOPMENT #
+#=====================================#
+
+## run/api: run the cmd/api application
+.PHONY: run
+run:
+	go run .
+
+#=====================================#
+# QUALITY CONTROL #
+#=====================================#
+
+## audit: tidy dependencies and format, vet and test all code
+
+## go fmt ./... : command to format all .go files in the project directory, according to the Go standard.
+## go vet ./... : runs a variety of analyzers which carry out static analysis of your code and warn you
+## go test -race -vet=off ./... : command to run all tests in the project directory
+## staticcheck tool : to carry out some additional static analysis checks.
+.PHONY: audit
+audit: vendor
+	@echo 'Formatting code...'
+	go fmt ./...
+	@echo 'Vetting code...'
+	go vet ./...
+	staticcheck ./...
+	@echo 'Running tests...'
+	go test -race -vet=off ./...
+
+## go mod tidy : prune any unused dependencies from the go.mod and go.sum files, and add any missing dependencies
+## go mod verify : check that the dependencies on your computer (located in your module cache located at $GOPATH/pkg/mod)
+## haven’t been changed since they were downloaded and that they match the cryptographic hashes in your go.sum file
+## go mod vendor: copy the necessary source code from your module cache into a new vendor directory in your project root
+.PHONY: vendor
+vendor:
+	@echo 'Tidying and verifying module dependencies...'
+	go mod tidy
+	go mod verify
+	@echo 'Vendoring dependencies...'
+	go mod vendor
--- main.go
+++ main.go
@@ -2,54 +2,78 @@ package main
 
 import (
 	"fmt"
-	"log"
+	"net/http"
+	"strconv"
+	"time"
 
-	// Import the redigo/redis package.
 	"github.com/gomodule/redigo/redis"
 )
 
-// Define a custom struct to hold Album data. Notice the struct tags?
-// These indicate to redigo how to assign the data from the reply into
-// the struct.
-type Album struct {
-	Title  string  `redis:"title"`
-	Artist string  `redis:"artist"`
-	Price  float64 `redis:"price"`
-	Likes  int     `redis:"likes"`
+func main() {
+	// Initialize a connection pool and assign it to the pool global
+	// variable.
+	pool = &redis.Pool{
+		MaxIdle:     10,
+		IdleTimeout: 240 * time.Second,
+		Dial: func() (redis.Conn, error) {
+			return redis.Dial("tcp", "localhost:6379")
+		},
+		TestOnBorrow: func(c redis.Conn, t time.Time) error {
+			if time.Since(t) < time.Minute {
+				return nil
+			}
+			_, err := c.Do("PING")
+			return err
+		},
+	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/album", showAlbum)
+	http.ListenAndServe(":80", mux)
 }
 
-func main() {
-	// Establish a connection to the Redis server listening on port
-	// 6379 of the local machine. 6379 is the default port, so unless
-	// you've already changed the Redis configuration file this should
-	// work.
-	conn, err := redis.Dial("tcp", ":6379")
-	if err != nil {
-		log.Fatal(err)
+func showAlbum(w http.ResponseWriter, r *http.Request) {
+	// Unless the request is using the GET method, return a 405 'Method
+	// Not Allowed' response.
+	if r.Method != http.MethodGet {
+		w.Header().Set("Allow", http.MethodGet)
+		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+		return
 	}
 
-	// Importantly, use defer to ensure the connection is always
-	// properly closed before exiting the main() function.
-	defer conn.Close()
-
-	// Fetch all album fields with the HGETALL command. Wrapping this
-	// in the redis.Values() function transforms the response into type
-	// []interface{}, which is the format we need to pass to
-	// redis.ScanStruct() in the next step.
-	values, err := redis.Values(conn.Do("HGETALL", "album:2"))
-	if err != nil {
-		log.Fatal(err)
+	// Retrieve the id from the request URL query string. If there is
+	// no id key in the query string then Get() will return an empty
+	// string. We check for this, returning a 400 Bad Request response
+	// if it's missing.
+	// id := r.Form.Get("id")
+	id := r.URL.Query().Get("id")
+	if id == "" {
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
 	}
 
-	// Create an instance of an Album struct and use redis.ScanStruct()
-	// to automatically unpack the data to the struct fields. This uses
-	// the struct tags to determine which data is mapped to which
-	// struct fields.
-	var album Album
-	err = redis.ScanStruct(values, &album)
-	if err != nil {
-		log.Fatal()
+	// Validate that the id is a valid integer by trying to convert it,
+	// returning a 400 Bad Request response if the conversion fails.
+	if _, err := strconv.Atoi(id); err != nil {
+		if id == "" {
+			http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+			return
+		}
 	}
 
-	fmt.Printf("%+v\n", album)
+	// Call the FindAlbum() function passing in the user-provided id.
+	// If there's no matching album found, return a 404 Not Found
+	// response. In the event of any other errors, return a 500
+	// Internal Server Error response.
+	bk, err := FindAlbum(id)
+	if err == ErrNoAlbum {
+		http.NotFound(w, r)
+		return
+	} else if err != nil {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+	}
+
+
+	// Write the album details as plain text to the client.
+	fmt.Fprintf(w, "%s by %s: £%.2f [%d likes] \n", bk.Title, bk.Artist, bk.Price, bk.Likes)
 }
